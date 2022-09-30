5b2fc12398e38c522f5631d262dc681715976497
jwatts41 <wattsjonovan@yahoo.com>
2022-09-30 16:53:32 -0400

    form data is getting through

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
 app.use(express.static('public'))
-
-
+app.use(express.urlencoded({ extended: true }))
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
