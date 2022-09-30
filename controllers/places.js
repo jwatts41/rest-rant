20018a649f49d9f95056eb3c8210c480c6959a24
jwatts41 <wattsjonovan@yahoo.com>
2022-09-30 14:41:32 -0400

    adding bootstrap grid to index page and also added images

--- controllers/places.js
+++ controllers/places.js
@@ -2,17 +2,17 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
     let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        name: 'Chipotle',
+        city: 'Durham',
+        state: 'NC',
+        cuisines: 'Mexican Grill',
+        pic: '/images/Chipotle.jpg'
       }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        name: 'Bar Taco',
+        city: 'Chapel Hill',
+        state: 'NC',
+        cuisines: 'Great Food',
+        pic: '/images/bartaco.jpg'
       }]
       
     res.render('places/index', {places })
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="/images/3drinks.jpg" alt="3 drinks" />
+            </div>
+            <div>
+            Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
             <a href="/places">
         <button className="btn-primary">Places Page</button>
             </a>
