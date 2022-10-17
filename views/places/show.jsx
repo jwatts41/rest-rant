8a19a06bc8221a6872515b0f87ab8694329d6ac7
jwatts41 <wattsjonovan@yahoo.com>
2022-10-17 15:51:34 -0400

    implementing the completing form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -19,6 +19,9 @@ function show (data) {
                       <strong>- { c.author }</strong>
                   </h3>
                   <h4>Rating: { c.stars }</h4>
+                  <form method="POST" action={ `/places/${data.place.id}/comment/${c.id}?_method=DELETE` }>
+                        <input type="submit" className="btn btn-danger" value="Delete Comment" />
+                    </form>
                   </div>
           )
       })
