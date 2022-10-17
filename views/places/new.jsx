19791c9cf60efcef451cf32ea433514522ca16ae
jwatts41 <wattsjonovan@yahoo.com>
2022-10-16 22:25:29 -0400

    updated schema with min and max in year founded

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,36 +6,56 @@ function new_form () {
         <Def>
           <main>
             <h1>Add a New Place</h1>
-            <form method ="POST" action="/places">
-                <div className="form-group">
+            <form method="POST" action="/places">
+            <div className="form-group">
                     <label htmlFor="name">Place Name</label>
-                    <input className="form-control" id="name" name="name" required/>
+                    <input className="form-control" 
+                    id="name" 
+                    name="name" 
+                    required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="name">Place Picture</label>
-                    <input className="form-control" type="url" id="pic" name="pic" />
+                    <label htmlFor="pic">Place Picture</label>
+                    <input className="form-control" 
+                    type="url" 
+                    id="pic" 
+                    name="pic"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="name">City</label>
-                    <input className="form-control" id="city" name="city" />
+                    <input className="form-control" 
+                    id="city" 
+                    name="city" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="name">State</label>
-                    <input className="form-control" id="state" name="state" />
+                    <input className="form-control" 
+                    id="state" 
+                    name="state" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="name">Cuisines</label>
-                    <input className="form-control" id="cuisines" name="cuisines" required/>
+                    <input className="form-control" 
+                    id="cuisines" 
+                    name="cuisines" 
+                    required />
                 </div>
                 <div className="form-group">
                     <label for="founded">Founded Year</label>
-                    <input className="form-control" id="founded" name="founded" />
+                    <input 
+                    type="number"
+                    className="form-control" 
+                    id="founded" 
+                    name="founded" 
+                    value={new Date().getFullYear()} />
                 </div>
-                <input className="btn btn-primary" type="submit" value="Add Place" />
+                <input className="btn btn-primary" 
+                type="submit" 
+                value="Add Place"/>
             </form>
           </main>
         </Def>
     )
-}
+} 
 
 module.exports = new_form
